6d30dd49ec5939adfc3288763a1c4102e29a0244
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2011-12-31 23:41:50 +0200

    add db config

--- pizza/settings.py
+++ pizza/settings.py
@@ -1,20 +1,21 @@
+# coding=utf-8
 # Django settings for pizza project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Yrjö Kari-Koskinen', 'ykk@peruna.fi'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'pizza',                      # Or path to database file if using sqlite3.
+        'USER': 'pizza',                      # Not used with sqlite3.
+        'PASSWORD': 'valkosipuli',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
