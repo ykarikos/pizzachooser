22459c28e1f5afbf37dea4c9dd2471124141c704
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2012-01-01 00:26:16 +0200

    rename filling to topping, add __unicode__ methods

--- pizza/chooser/models.py
+++ pizza/chooser/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
-class Filling(models.Model):
+class Topping(models.Model):
     name = models.CharField(max_length = 50)
+    def __unicode__(self):
+        return self.name
 
 class Pizzeria(models.Model):
     name = models.CharField(max_length = 50)
+    def __unicode__(self):
+        return self.name
 
 class Pizza(models.Model):
     name = models.CharField(max_length = 50)
     number = models.IntegerField()
-    fillings = models.ManyToManyField(Filling)
+    toppings = models.ManyToManyField(Topping)
     pizzeria = models.ForeignKey(Pizzeria)
+    def __unicode__(self):
+        return str(self.number) + ". " + self.name
 
 
