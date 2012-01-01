19922e275a02f9ed38109519b90a35b62ef6d0a4
Yrjö Kari-Koskinen <ykk@peruna.fi>
2012-01-01 14:43:31 +0200

    add toppings chooser, make html more structured, add static dir

--- pizza/chooser/views.py
+++ pizza/chooser/views.py
@@ -4,4 +4,5 @@ from chooser.models import Pizza, Pizzeria, Topping
 def home(request):
     return render_to_response('chooser/index.html', {
             'pizzerias': Pizzeria.objects.all(),
+            'toppings': sorted(Topping.objects.all(), lambda a,b: cmp(a.name, b.name))
             })
