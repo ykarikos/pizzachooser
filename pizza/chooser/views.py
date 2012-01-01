4ab64697aacf584388446bcb46c5c07b283aff4a
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2012-01-01 02:32:42 +0200

    view all data

--- pizza/chooser/views.py
+++ pizza/chooser/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render_to_response
 from chooser.models import Pizza, Pizzeria, Topping
 
 def home(request):
-    pizzerias = Pizzeria.objects.all()
     return render_to_response('chooser/index.html', {
-            'pizzerias': pizzerias,
+            'pizzerias': Pizzeria.objects.all(),
             })
